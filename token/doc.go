61a7c0928006d21e26c2de4e1cea448e2057577b
Koichi Shiraishi <zchee.io@gmail.com>
2019-07-15 03:38:39 +0900

    token: add doc.go

---
+++ token/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2019 The go-yaml Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package token defines constants representing the lexical tokens of the YAML
+// language and basic operations on tokens (printing, predicates).
+package token
