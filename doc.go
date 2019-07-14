733d83ce59440b845a5edfaa0eeabe67b513f473
Koichi Shiraishi <zchee.io@gmail.com>
2019-07-15 03:51:53 +0900

    doc: add top lovel package synopsis

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 The go-yaml Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package go-yaml implements a YAML AST parser for Go.
+package yaml
